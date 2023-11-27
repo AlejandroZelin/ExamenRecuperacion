d5ff863da7728de70480be3a6637411705d86050
AlejandroZelin <alejandrozelin2@gmail.com>
2023-11-27 15:38:25 +0000

    Initial commit

---
+++ Assets/Scripts/MovimientoYDemas.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoYDemas : MonoBehaviour
+{
+    Rigidbody2D fisicas;
+    public float movX, movY;
+    public float magnitudVelocidad = 3;
+    private Color colorOriginal;
+    SpriteRenderer sprite;
+    private float tiempoDetenido = 2.0f;
+    private float tiempoColision;
+    private bool detener = false;
+    void Start()
+    {
+        fisicas = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detener && Time.time - tiempoColision >= tiempoDetenido)
+        {
+            Resetcolor();
+            detener = false;
+        }
+
+        if (!detener)
+        {
+            float movimientoHorizontal = Input.GetAxis("Horizontal");
+            float movimientoVertical = Input.GetAxis("Vertical");
+            Vector2 movimiento = new Vector2(movimientoHorizontal * magnitudVelocidad , fisicas.velocity.y);
+
+            fisicas.velocity = movimiento ;
+        }
+        else
+        {
+            fisicas.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Zona"))
+        {
+            sprite.color = Color.red;
+            detener = true;
+            tiempoColision = Time.time;
+        }
+    }
+
+    private void Resetcolor()
+    {
+        sprite.color = colorOriginal;
+    }
+}
---
+++ Assets/Scripts/ObjetoEnMovimiento.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetoEnMovimiento : MonoBehaviour
+{
+    private float movY;
+    bool haciaUnLado;
+    public float Velocidad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vigilarDireccion();
+        mover();
+    }
+
+    void vigilarDireccion() 
+    {
+        if (transform.position.x > -10)
+        {
+            haciaUnLado = false;
+        }
+
+        else if (transform.position.x < -2)
+        {
+            haciaUnLado = true;
+        } 
+    }
+    void mover()
+    {
+        if (haciaUnLado == true)
+        {
+            transform.Translate(Velocidad * Vector2.left * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Velocidad * Vector2.right * Time.deltaTime);
+        }
+    }
+}
